50567b4483b860faebb5b77a33cf81a10c2bff35
Ritxam <85738906+Ritxam@users.noreply.github.com>
2021-09-11 14:28:07 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+int main()
+{
+
+  int a,b,c;
+
+  std::cout<<"enter a :";
+  std::cin>>a;
+  
+  std::cout<<std::endl;
+
+  std::cout<<"enter b :";
+  std::cin>>b;
+
+  std::cout<<std::endl;
+
+  c=a+b;
+  
+
+  std::cout<<"sum of a and b is :"<<c<<std::endl;
+
+  return 0;
+}
